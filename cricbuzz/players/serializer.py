992d394a1d3cdfe93b0d1ea83e4ec41cc603fc82
Jui Kamone <juikamone59399@gmail.com>
2024-05-19 14:27:50 +0530

    initial commit

--- cricbuzz/players/serializer.py
+++
@@ -1,9 +0,0 @@
-from players.models import PlayersModel
-from rest_framework import serializers
-
-
-class PlayerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PlayersModel
-        fields = '__all__'
-        
